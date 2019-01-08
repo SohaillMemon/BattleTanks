e37ef016e813f196eb7e9074698235bcbc6452e8
sohaill <sohailmemon996@gmail.com>
2019-01-08 02:24:03 -0800

    battlegroundmapinitialcommit

---
+++ Battleltank/Source/Battleltank/BattleltankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleltankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLELTANK_API ABattleltankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
