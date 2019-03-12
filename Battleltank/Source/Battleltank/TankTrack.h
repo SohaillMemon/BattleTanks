21c5cf532593210cc8e9b4148ed341c908061000
sohaill <sohailmemon996@gmail.com>
2019-03-11 23:03:13 -0700

    Friction And Force

---
+++ Battleltank/Source/Battleltank/TankTrack.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLELTANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000;
+	
+};
